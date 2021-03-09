1cce72a3c7443345e85e40a65423aa24c53f0f91
Chris Maki <chris.maki@ripcitysoftware.com>
2021-03-08 19:08:02 -0800

    add filename to set-version

--- commands/version.py
+++ commands/version.py
@@ -9,10 +9,10 @@ def cli():
     pass
 
 
-def setup_python(version: str):
+def setup_python(version: str, file: str):
     print(f"In python_setup, version is {version}")
     
-    version_path = Path.cwd() / "app" / "version.py"
+    version_path = Path.cwd() / file
     with open(version_path, "w+") as out:
         out.write(f"VERSION = \"{version}\"")
         out.write("\n")
@@ -21,12 +21,13 @@ def setup_python(version: str):
 #  cm-toolkit version --language python --version "$(cat VERSION_INFO)"
 @click.command()
 @click.option("-l", '--language',
-              type=click.Choice(['python'], case_sensitive=False), required=True)
+              type=click.Choice(['python', 'angular'], case_sensitive=False), required=True)
 @click.option("-v", "--version", required=True)
-def set_version(language: str, version: str):
+@click.option("-f", "--file", required=True, help="Path to file relative to the project root directory.")
+def set_version(language: str, version: str, file: str):
     print(f"{language=}, {version=}")
     if "python" == language:
-        setup_python(version)
+        setup_python(version, file)
 
 
 cli.add_command(set_version)
